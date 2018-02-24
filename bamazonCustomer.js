127150a8629ad6fcd2d98da34b740c5fb274b14e
John Cordano <jecordano@yahoo.com>
2018-02-23 20:03:34 -0800

    Added validation of user input.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,17 +12,17 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    // Run the userOrder function after the connection is established
+    // console.log("connected as id " + connection.threadId);
+    // Run the userOrder function after the connection is established.
     userOrder();
 });
 
-// Create the table of products 
+// Create the table of products. 
 var table = new Table({
     head: ['Item Id', 'Product', 'Price'],
     colWidths: [15, 15, 15]
 });
-// Populate the table with data 
+// Populate the table with data. 
 table.push(
     ['1', 'Monopoly', '20.50'], 
     ['2', 'Checkers', '10.25'], 
@@ -37,35 +37,61 @@ table.push(
 );
 
 function userOrder() {
-    // Query the database for all products the user can purchase
+    // Query the database for all products the user can purchase.
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
         // console.log(results);
-        // Display the table of products the user can purchase
+        // Display the table of products the user can purchase.
         console.log(table.toString());
-        // Prompt the user to select an item and an item amount
+        // Prompt the user to select an item and an item amount.
         inquirer
             .prompt([{
                     name: "purchase",
                     type: "input",
-                    message: "What is the Id of the item you want to purchase?"
+                    message: "What is the Id of the item you want to purchase? (Enter 'q' to exit)",
+                    validate: function(value) {
+                        // Exit the application when the user presses the 'q' key.
+                        if ((value) === "q") {
+                            console.log("\n  Exiting...");
+                            process.exit();
+                        }
+                        // Display the appropropriate message when the user presses a non-number key, including the space bar, Enter, and a special character. 
+                        else if (isNaN(parseInt(value)) === true) {
+                            console.log("\n  You must enter a number.");
+                            return false;
+                        }
+                        return true;    
+                    }
                 },
                 {
                     name: "quantity",
                     type: "input",
-                    message: "How many would you like?"
+                    message: "How many would you like? (Enter 'q' to exit)",
+                    validate: function(value) {
+                        // Exit the application when the user presses the 'q' key.
+                        if ((value) === "q") {
+                            console.log("\n  Exiting...");
+                            process.exit();
+                        }
+                        // Display the appropropriate message when the user presses a non-number key, including the space bar, Enter, and a special character.
+                        else if (isNaN(parseInt(value)) === true) {
+                            console.log("\n  You must enter a number.");
+                            return false;
+                        }
+                        return true;
+                    }
                 }
             ]).then(function(answer) {
-                // Retrieve information about the purchased item
+                // Retrieve information about the purchased item.
                 var purchasedItem;
                 for (var i = 0; i < results.length; i++) {
                     if (results[i].item_id == answer.purchase) {
                         purchasedItem = results[i];
                     }
                 }
-                // Determine if the purchase quantity is less than the stock quantity
+                // Determine if the purchase quantity is less than the stock quantity.
                 if (parseInt(answer.quantity) < parseInt(purchasedItem.stock_quantity)) {
-                    // Decrease the stock quantity in the database by the purchase quantity, provide the user information about the purchase, and start over
+                    // Decrease the stock quantity in the database by the purchase quantity, provide the user information about the purchase, and start over.
                     connection.query(
                         "UPDATE products SET ? WHERE ?", [{
                                 stock_quantity: (purchasedItem.stock_quantity - answer.quantity)
@@ -76,18 +102,21 @@ function userOrder() {
                         ],
                         function(error) {
                             if (error) throw err;
-                            console.log("\nItem you purchased: " + purchasedItem.product_name + "\nQuantity of this item you purchased: " + answer.quantity);
+                            console.log("\n  Item you purchased: " + purchasedItem.product_name + "\n  Quantity of this item you purchased: " + answer.quantity);
                             console.log("=============================================================");
 
                             userOrder();
                         }
                     );
                 } else {
-                    // If the order quantity is to high, start over
-                    console.log("We have insufficient quantities to fullfill your order.");
+                    // If the order quantity is to high, display an appropriate message, and start over.
+                    console.log("\n  We have insufficient quantities to fullfill your order.");
                     console.log("=============================================================");
                     userOrder();
                 }
             });
     });
 }
+
+// connection.end();
+
